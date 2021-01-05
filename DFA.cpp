404546a60cd26470b767862e1191a94ca7ccda3b
yonitoo <36246462+yonitoo@users.noreply.github.com>
2021-01-05 05:04:25 +0200

    Add "The Big 4", setters, getters, add/remove methods, check if a word can be recognized by L and comments.

--- DFA.cpp
+++ DFA.cpp
@@ -1,1 +1,163 @@
 #include "DFA.h"
+
+void DFA::copy(const DFA& other) {
+
+    this->alphabet = other.alphabet;
+    this->states = other.states;
+    this->delta = other.delta;
+    this->qs = qs;
+    this->finalStates = other.finalStates;
+}
+
+void DFA::erase() {
+
+}
+
+DFA::DFA() : alphabet (std::unordered_set<char>()), states (std::unordered_set<std::string>()), 
+            delta (std::map<std::pair<std::string, char>, std::string>()), qs(nullptr), 
+            finalStates(std::unordered_set<std::string>()) {
+
+}
+
+DFA::DFA(const std::unordered_set<char>& alphabet, const std::unordered_set<std::string>& states, 
+        const std::map<std::pair<std::string, char>, std::string>& delta, const std::string& qs, 
+        const std::unordered_set<std::string>& finalStates) {
+    
+    this->alphabet = alphabet;
+    this->states = states;
+    this->delta = delta;
+    this->qs = qs;
+    this->finalStates = finalStates;
+
+}
+
+DFA::DFA(const DFA& other) {
+
+    copy(other);
+}
+
+DFA& DFA::operator= (const DFA& other) {
+
+    if(this != &other) {
+
+        erase();
+        copy(other);
+    }
+
+    return *this;
+}
+
+DFA::~DFA() {
+
+}
+
+void DFA::addLetter(const char& letter) {
+
+    this->alphabet.insert(letter);
+}
+
+void DFA::addState(const std::string& state) {
+
+    this->states.insert(state);
+}
+
+void DFA::addTransition(const std::pair<std::string, char>& sourceLetter, 
+                        const std::string& destination) {
+
+    this->states.insert(sourceLetter.first);
+    this->states.insert(destination);
+    this->delta[sourceLetter] = destination;
+}
+
+void DFA::addFinalState(const std::string& final) {
+
+    this->finalStates.insert(final);
+}
+
+bool DFA::removeLetter(const char& letter) {
+
+    this->alphabet.erase(letter);
+}
+
+bool DFA::removeState(const std::string& state) {
+
+    this->states.erase(state);
+}
+
+bool DFA::removeTransition(const std::pair<std::string, char>& sourceLetter, 
+                            const std::string& destination) {
+
+    this->delta.erase(sourceLetter);
+}
+
+bool DFA::removeFinalState(const std::string& final) {
+
+    this->finalStates.erase(final);
+}
+
+void DFA::setAlphabet(const std::unordered_set<char>& alphabet) {
+
+    this->alphabet = alphabet;
+}
+
+void DFA::setStates(const std::unordered_set<std::string>& states) {
+
+    this->states = states;
+}
+
+void DFA::setDelta(const std::map<std::pair<std::string, char>, std::string>& delta) {
+
+    this->delta = delta;
+}
+
+void DFA::setQs(const std::string& qs) {
+
+    this->qs = qs;
+}
+
+void DFA::setFinalStates(const std::unordered_set<std::string>& finalStates) {
+
+    this->finalStates = finalStates;
+}
+
+std::unordered_set<char> DFA::getAlphabet() const {
+
+    return this->alphabet;
+}
+
+std::unordered_set<std::string> DFA::getStates() const {
+
+    return this->states;
+}
+
+std::map<std::pair<std::string, char>, std::string> DFA::getDelta() const {
+
+    return this->delta;
+}
+
+std::string DFA::getQs() const {
+
+    return this->qs;
+}
+
+std::unordered_set<std::string> DFA::getFinalStates() const {
+
+    return this->finalStates;
+}
+
+bool DFA::canBeRecognized(const std::string& word) {
+
+    int length = word.size();
+    std::string currentState = this->qs;
+    for(int i = 0 ; i < length ; i++) {
+        
+        if(!this->delta.count(std::make_pair(currentState, word[i]))) {
+
+            return false;
+        }
+        
+        currentState = this->delta[std::make_pair(currentState, word[i])];
+    }
+    
+    return this->finalStates.find(currentState) != this->finalStates.end();
+}
--- DFA.h
+++ DFA.h
@@ -1,15 +1,89 @@
 #ifndef DFA_H
 #define DFA_H
 
+#include <unordered_set>
+#include <map>
+#include <utility>
+#include <string>
+
+//Автоматът е наредена петорка (Σ, Q, 𝛿, qstart, F)
 class DFA {
 
     private: 
+        //Σ
+        std::unordered_set<char> alphabet;
+        //Q 
+        std::unordered_set<std::string> states; 
+        //𝛿
+        std::map<std::pair<std::string, char>, std::string> delta;
+        //Initial state
+        std::string qs;
+        //F
+        std::unordered_set<std::string> finalStates; //F
 
-
+        void copy(const DFA&);
+        void erase();
 
     public:
+        //Default constructor
+        DFA();
+        //Constructor with arguments
+        DFA(const std::unordered_set<char>&, const std::unordered_set<std::string>&, 
+            const std::map<std::pair<std::string, char>, std::string>&, const std::string&, 
+            const std::unordered_set<std::string>&);
+        //Copy constructor
+        DFA(const DFA&);
+        //Operator=
+        DFA& operator= (const DFA&);
+        //Destructor
+        ~DFA();
 
-        
+        //Add functions
+        //Add character to the alphabet
+        void addLetter(const char&);
+        //Add state to the set of states
+        void addState(const std::string&);
+        //Add transition to the delta function
+        void addTransition(const std::pair<std::string, char>&, const std::string&);
+        //Add final state to the set of final states
+        void addFinalState(const std::string&);
+
+        //Remove functions
+        //Remove letter from the alphabet       
+        bool removeLetter(const char&);
+        //Remove state from the set of states
+        bool removeState(const std::string&);
+        //Remove transition from the delta function
+        bool removeTransition(const std::pair<std::string, char>&, const std::string&);
+        //Remove final state from the set of final states
+        bool removeFinalState(const std::string&);
+
+        //Setters
+        //Set the letters of the alphabet
+        void setAlphabet(const std::unordered_set<char>&);
+        //Set the set of states
+        void setStates(const std::unordered_set<std::string>&); 
+        //Set the delta function
+        void setDelta(const std::map<std::pair<std::string, char>, std::string>&);
+        //Set the initial state
+        void setQs(const std::string&);
+        //Set the set of final states
+        void setFinalStates(const std::unordered_set<std::string>&);
+
+        //Getters
+        //Get the letters of the alphabet
+        std::unordered_set<char> getAlphabet() const;
+        //Get the set of states
+        std::unordered_set<std::string> getStates() const; 
+        //Get the delta function
+        std::map<std::pair<std::string, char>, std::string> getDelta() const;
+        //Get the initial state
+        std::string getQs() const;
+        //Get the set of final states
+        std::unordered_set<std::string> getFinalStates() const;
+
+        //Check if a word can be recognized by the language of the DFA
+        bool canBeRecognized(const std::string&);
 };
 
 #endif
