8f16f8b6c3c4fb28444b2c21c85d040d1d8e8258
yonitoo <36246462+yonitoo@users.noreply.github.com>
2021-01-07 03:47:48 +0200

    Test the Alphabet methods addLetter and removeLetter OK.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
+#include "Alphabet.cpp"
 #include "DFA.cpp"
 
 int main() {
 
+    Alphabet alphabet;
+    alphabet.addLetter('v');
+    alphabet.addLetter('i');
+    alphabet.addLetter('k');
     DFA dfa;
-    dfa.addLetter('v');
-    dfa.addLetter('i');
-    dfa.addLetter('k');
+    dfa.setAlphabet(alphabet);
     dfa.addState("qs");
     dfa.setQs("qs");
     dfa.addTransition(std::make_pair("qs", 'v'), "qs"); 
     dfa.addTransition(std::make_pair("qs", 'i'), "qs"); 
     dfa.addTransition(std::make_pair("qs", 'k'), "qs");
     dfa.addFinalState("qs");
+    if(dfa.canBeRecognized("vik")) {
+
+        std::cout << "Successfully recognized the word!" << std::endl;
+    }
+    else {
+
+        std::cout << "The DFA doesn't recognize the word!" << std::endl;
+    }
+
+    dfa.removeLetter('k');
     if(dfa.canBeRecognized("vik")) {
 
         std::cout << "Successfully recognized the word!" << std::endl;
