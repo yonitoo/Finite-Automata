7d19d84f9b1a04a207890293e61d4d350723077c
yonitoo <36246462+yonitoo@users.noreply.github.com>
2021-01-07 03:47:03 +0200

    Add some ideas for implementing automata methods.

--- DFA.cpp
+++ DFA.cpp
@@ -1,5 +1,6 @@
 #include "DFA.h"
 
+//template <typename T>
 void DFA::copy(const DFA& other) {
 
     this->alphabet = other.alphabet;
@@ -9,12 +10,14 @@ void DFA::copy(const DFA& other) {
     this->finalStates = other.finalStates;
 }
 
+//template <typename T>
 DFA::DFA() : alphabet (Alphabet()), states (std::unordered_set<std::string>()), 
             delta (std::map<std::pair<std::string, char>, std::string>()), qs(""), 
             finalStates(std::unordered_set<std::string>()) {
 
 }
 
+//template <typename T>
 DFA::DFA(const Alphabet& alphabet, const std::unordered_set<std::string>& states, 
         const std::map<std::pair<std::string, char>, std::string>& delta, const std::string& qs, 
         const std::unordered_set<std::string>& finalStates) {
@@ -27,11 +30,13 @@ DFA::DFA(const Alphabet& alphabet, const std::unordered_set<std::string>& states
 
 }
 
+//template <typename T>
 DFA::DFA(const DFA& other) {
 
     copy(other);
 }
 
+//template <typename T>
 DFA& DFA::operator= (const DFA& other) {
 
     if(this != &other) {
@@ -42,20 +47,24 @@ DFA& DFA::operator= (const DFA& other) {
     return *this;
 }
 
+//template <typename T>
 DFA::~DFA() {
 
 }
 
-void DFA::addLetter(const char& letter) {
+//template <typename T>
+Alphabet& DFA::addLetter(const char& letter) {
 
-    this->alphabet.addLetter(letter);
+    return this->alphabet.addLetter(letter);
 }
 
+//template <typename T>
 void DFA::addState(const std::string& state) {
 
     this->states.insert(state);
 }
 
+//template <typename T>
 void DFA::addTransition(const std::pair<std::string, char>& sourceLetter, 
                         const std::string& destination) {
 
@@ -64,21 +73,43 @@ void DFA::addTransition(const std::pair<std::string, char>& sourceLetter,
     this->delta[sourceLetter] = destination;
 }
 
+//template <typename T>
 void DFA::addFinalState(const std::string& final) {
 
     this->finalStates.insert(final);
 }
 
-void DFA::removeLetter(const char& letter) {
+//template <typename T>
+Alphabet& DFA::removeLetter(const char& letter) {
 
+    int size = this->alphabet.getLetters().size();
     this->alphabet.removeLetter(letter);
+    //Check if the letter was not in the alphabet
+    if(this->alphabet.getLetters().size() == size) {
+
+        return this->alphabet;
+    }
+    //Iterate over the map and delete the transitions, related to this letter
+    for (std::map<std::pair<std::string, char>, std::string>::iterator it = this->delta.begin(); 
+            it != this->delta.end(); ) {
+
+        if(it->first.second == letter) {
+
+            this->delta.erase(it++);
+        }
+        else ++it;
+    }
+
+    return this->alphabet;
 }
 
+//template <typename T>
 void DFA::removeState(const std::string& state) {
 
     this->states.erase(state);
 }
 
+//template <typename T>
 void DFA::removeTransition(const std::pair<std::string, char>& sourceLetter, 
                             const std::string& destination) {
 
@@ -86,61 +117,73 @@ void DFA::removeTransition(const std::pair<std::string, char>& sourceLetter,
     this->delta.erase(sourceLetter);
 }
 
+//template <typename T>
 void DFA::removeFinalState(const std::string& final) {
 
     this->finalStates.erase(final);
 }
 
+//template <typename T>
 void DFA::setAlphabet(const Alphabet& alphabet) {
 
     this->alphabet = alphabet;
 }
 
+//template <typename T>
 void DFA::setStates(const std::unordered_set<std::string>& states) {
 
     this->states = states;
 }
 
+//template <typename T>
 void DFA::setDelta(const std::map<std::pair<std::string, char>, std::string>& delta) {
 
     this->delta = delta;
 }
 
+//template <typename T>
 void DFA::setQs(const std::string& qs) {
 
     this->qs = qs;
 }
 
+//template <typename T>
 void DFA::setFinalStates(const std::unordered_set<std::string>& finalStates) {
 
     this->finalStates = finalStates;
 }
 
+//template <typename T>
 Alphabet DFA::getAlphabet() const {
 
     return this->alphabet;
 }
 
+//template <typename T>
 std::unordered_set<std::string> DFA::getStates() const {
 
     return this->states;
 }
 
+//template <typename T>
 std::map<std::pair<std::string, char>, std::string> DFA::getDelta() const {
 
     return this->delta;
 }
 
+//template <typename T>
 std::string DFA::getQs() const {
 
     return this->qs;
 }
 
+//template <typename T>
 std::unordered_set<std::string> DFA::getFinalStates() const {
 
     return this->finalStates;
 }
 
+//template <typename T>
 bool DFA::canBeRecognized(const std::string& word) {
 
     int length = word.size();
@@ -156,4 +199,94 @@ bool DFA::canBeRecognized(const std::string& word) {
     }
     
     return this->finalStates.find(currentState) != this->finalStates.end();
+}
+
+//DFA<std::pair<std::string, std::string>& DFA<std::string>::uni(DFA<std::string>& dfa)
+/*DFA& DFA::uni(const DFA& dfa) {
+
+    //<std::pair<std::string, std::string>>
+    DFA result;
+    result.setQs(std::make_pair(this->getQs(), dfa.getQs()));
+    for (std::unordered_set<char>::iterator it = this->alphabet.getLetters().begin(); 
+            it != this->alphabet.getLetters().end(); it++) {
+
+        result.addLetter(*it);
+    }
+
+    for (std::unordered_set<char>::iterator it = dfa.getAlphabet().getLetters().begin(); 
+            it != dfa.getAlphabet().getLetters().end(); it++) {
+
+        result.addLetter(*it);
+    }
+    //състояния
+    //преходи
+    for (std::unordered_set<std::pair<std::string, std::string>>::iterator it = result.getStates().begin(); 
+            it != result.getStates().end(); it++) {
+
+        if((this->finalStates.find(it->first) != this->finalStates.end()) ||
+           (dfa.getFinalStates().find(it->second) != dfa.getFinalStates().end())) {
+
+            result.addFinalState(*it);
+        }
+    }
+    return result;
+}
+
+//template <typename T>
+DFA& DFA::intersection(const DFA&) {
+
+    //<std::pair<std::string, std::string>>
+    DFA result;
+    result.setQs(std::make_pair(this->getQs(), dfa.getQs()));
+    for (std::unordered_set<char>::iterator it = this->alphabet.getLetters().begin(); 
+            it != this->alphabet.getLetters().end(); it++) {
+
+        result.addLetter(*it);
+    }
+
+    for (std::unordered_set<char>::iterator it = dfa.getAlphabet().getLetters().begin(); 
+            it != dfa.getAlphabet().getLetters().end(); it++) {
+
+        result.addLetter(*it);
+    }
+    //състояния
+    //преходи
+    for (std::unordered_set<std::pair<std::string, std::string>>::iterator it = result.getStates().begin(); 
+            it != result.getStates().end(); it++) {
+
+        if((this->finalStates.find(it->first) != this->finalStates.end()) &&
+           (dfa.getFinalStates().find(it->second) != dfa.getFinalStates().end())) {
+
+            result.addFinalState(*it);
+        }
+    }
+    return result;
+}        */
+
+//template <typename T>
+DFA& DFA::concatenation(const DFA&) {
+
+    
+}   
+
+//template <typename T>
+DFA& DFA::iteration() {
+
+}
+
+//template <typename T>
+DFA& DFA::addition(const DFA&)  {
+    
+    //обратните финални състояния
+}   
+
+//template <typename T>
+void DFA::print() const {
+
+}
+
+//template <typename T>
+std::string DFA::transform() {
+
+
 }
--- DFA.h
+++ DFA.h
@@ -8,17 +8,23 @@
 #include "Alphabet.h"
 
 //Автоматът е наредена петорка (Σ, Q, 𝛿, qstart, F)
+//template <typename T>
 class DFA {
 
     private: 
         //Σ
         Alphabet alphabet;
+        //std::unordered_set<T> states;
         //Q 
         std::unordered_set<std::string> states; 
+        //A TEMPLATE IDEA T = string | pair<string, string>
+        //std::map<std::pair<T, char>, T> delta;
         //𝛿
         std::map<std::pair<std::string, char>, std::string> delta;
+        //T qs;
         //Initial state
         std::string qs;
+        //std::unordered_set<T> finalStates;
         //F
         std::unordered_set<std::string> finalStates;
 
@@ -28,6 +34,9 @@ class DFA {
     public:
         //Default constructor
         DFA();
+        //DFA(const Alphabet&, const std::unordered_set<T>&, 
+        //    const std::map<std::pair<T, char>, T>&, const T&, 
+        //    const std::unordered_set<T>&);
         //Constructor with arguments
         DFA(const Alphabet&, const std::unordered_set<std::string>&, 
             const std::map<std::pair<std::string, char>, std::string>&, const std::string&, 
@@ -41,45 +50,59 @@ class DFA {
 
         //Add functions
         //Add character to the alphabet
-        void addLetter(const char&);
+        Alphabet& addLetter(const char&);
+        //void addState(const T&);
         //Add state to the set of states
         void addState(const std::string&);
+        //void addTransition(const std::pair<T, char>&, const T&);
         //Add transition to the delta function
         void addTransition(const std::pair<std::string, char>&, const std::string&);
+        //void addFinalState(const T&);
         //Add final state to the set of final states
         void addFinalState(const std::string&);
 
         //Remove functions
         //Remove letter from the alphabet       
-        void removeLetter(const char&);
+        Alphabet& removeLetter(const char&);
+        //void removeState(const T&);
         //Remove state from the set of states
         void removeState(const std::string&);
+        //void removeTransition(const std::pair<T, char>&, const T&);
         //Remove transition from the delta function
         void removeTransition(const std::pair<std::string, char>&, const std::string&);
+        //void removeFinalState(const T&);
         //Remove final state from the set of final states
         void removeFinalState(const std::string&);
 
         //Setters
         //Set the letters of the alphabet
         void setAlphabet(const Alphabet&);
+        //void setStates(const std::unordered_set<T>&); 
         //Set the set of states
         void setStates(const std::unordered_set<std::string>&); 
+        //void setDelta(const std::map<std::pair<T, char>, T>&);
         //Set the delta function
         void setDelta(const std::map<std::pair<std::string, char>, std::string>&);
+        //void setQs(const T&);
         //Set the initial state
         void setQs(const std::string&);
+        //void setFinalStates(const std::unordered_set<T>&);
         //Set the set of final states
         void setFinalStates(const std::unordered_set<std::string>&);
 
         //Getters
         //Get the letters of the alphabet
         Alphabet getAlphabet() const;
+        //std::unordered_set<T> getStates() const; 
         //Get the set of states
         std::unordered_set<std::string> getStates() const; 
+        //std::map<std::pair<T, char>, T> getDelta() const;
         //Get the delta function
         std::map<std::pair<std::string, char>, std::string> getDelta() const;
+        //T getQs() const;
         //Get the initial state
         std::string getQs() const;
+        //std::unordered_set<T> getFinalStates() const;
         //Get the set of final states
         std::unordered_set<std::string> getFinalStates() const;
 
